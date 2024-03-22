7604bdb016e16cef7beaaff345c43b11a71a417e
irvansaputra-irsa <irvansaputra234@gmail.com>
2024-03-23 00:07:23 +0700

    fix: baseurl to host at vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
-    baseURL: "http://localhost:3000/",
+    // baseURL: "http://localhost:3000/",
+    baseURL:
+      "https://company-profile-git-main-irvans-projects-07e5d8bd.vercel.app/",
   },
 };
